c6d34e2d4f1b40c1916af64e3ad3bba58f427806
kauan alcantara dos santos <74793317+plocicity@users.noreply.github.com>
2023-03-16 20:53:36 -0300

    Update aws_extensions.py

--- aws_extensions.py
+++ aws_extensions.py
@@ -2,33 +2,96 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-def all_objects_from_bucket(bucketName):
-    bucket = s3.Bucket(bucketName)
-    return bucket.objects
+const fs = require('fs');
+ 
+async function uploadFile(fileName, filePath, mimeType) {
+    const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+    const fileContent = fs.readFileSync(filePath);
+ 
+    const params = {
+        Bucket: NAME.env.AWS_S3_BUCKET,
+        Key: fileName,
+        Body: fileContent,
+        //ContentType: mimeType//geralmente se acha sozinho
+    };
+ 
+    const data = await s3.upload(params).promise();
+    return data.Location;
+}
+JavaScript
+const AWS = require('aws-sdk');
+const fs = require('fs');
 
-def objects_from_bucket_per_page(bucketName, page_size):
-    return s3.Bucket(bucketName).objects.page_size(page_size)
+async function uploadFile(fileName, filePath, mimeType) {
+    const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+    const fileContent = fs.readFileSync(filePath);
 
-def count_objects(objects):
-    count=0
-    for x in objects.all(): 
-        count+=1
-    return count
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: fileName,
+        Body: fileContent,
+        //ContentType: mimeType//geralmente se acha sozinho
+    };
 
-def get_object_summary(bucket, key):
-    return s3.ObjectSummary(bucket, key)
+    const data = await s3.upload(params).promise();
+    return data.Location;
+}
 
-def data_from_object(bucketName, key):
-    obj = s3.Object(bucketName,key)
-    return obj.content_type
+async function listObjects(filter) {
+    const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Prefix: decodeURIComponent(filter)
+    };
 
-def link_from_object(bucketName, key):
-    return 'https://%s.s3.amazonaws.com/%s' % (bucketName, key)
+    const result = await s3.listObjectsV2(params).promise();
+    return result.Contents.map(item => item.Key);
+}
 
-def delete_object_if_exists(bucketName, key):
-    obj = s3.Object(bucketName,key)
+module.exports = { uploadFile, listObjects }
 
-    if obj is None:
-        return
-    
-    obj.delete()
+const AWS = require('aws-sdk');
+const fs = require('fs');
+ 
+async function uploadFile(fileName, filePath, mimeType) {
+    const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+    const fileContent = fs.readFileSync(filePath);
+ 
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: fileName,
+        Body: fileContent,
+        //ContentType: mimeType//geralmente se acha sozinho
+    };
+ 
+    const data = await s3.upload(params).promise();
+    return data.Location;
+}
+ 
+async function listObjects(filter) {
+    const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Prefix: decodeURIComponent(filter)
+    };
+ 
+    const result = await s3.listObjectsV2(params).promise();
+    return result.Contents.map(item => item.Key);
+}
+ 
+module.exports = { uploadFile, listObjects }
+
+
+
+router.post('/', (req, res, next) => {
+  const formidable = require('formidable');
+  const form = new formidable.IncomingForm();
+ 
+  form.parse(req, async (err, fields, files) => {
+    const s3Client = require('../s3Client');
+    const url = await s3Client.uploadFile(files.filetoupload.name, files.filetoupload.path);
+    res.send(`File uploaded at ${https://fakestoreapi.com/docs. }`);
+  });
+});
+
+
